239b749567ac99c8a24c1a02a217de11b4717073
CryptoRoshan <118432414+tinyphoniex@users.noreply.github.com>
2024-02-19 03:55:30 +0900

    change footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,30 +1,41 @@
 import { Link } from 'components/Link';
+import Image from 'next/image';
+import {
+  // Avatar, 
+  DioneProtocolIcon,
+} from 'assets';
 import styles from './styles.module.scss';
 
 const Footer = () => (
   <div className={styles.footer_container}>
-    <div className="flex sm:flex-row flex-col sm:w-full w-1/2 mx-auto">
-      <Link href="/" className={styles.footer_link} needArrow={false}>
-        <p className="sm:text-base text-xs">
-          ©
-          {' '}
-          {new Date().getFullYear()}
-          {' '}
-          Dione Protocol LLC
-        </p>
-      </Link>
-      <div className="flex sm:flex-row flex-col justify-start">
-        <p className="sm:text-base text-xs">• All Rights Reserved</p>
-        <Link href="/services" className={styles.footer_link} needArrow={false}>
+    <div className="flex sm:flex-row gap-8 justify-between items-center flex-col sm:w-full mx-auto">
+      <div className="flex sm:flex-row flex-col gap-2 items-center">
+        <Image src={DioneProtocolIcon} alt="logo" width={36} height={36} />
+        <p>Powered by Dione Protocol</p>
+      </div>
+      <div className="flex sm:flex-row items-center flex-col">
+        <Link href="/" className={styles.footer_link} needArrow={false}>
           <p className="sm:text-base text-xs">
-            • Term of Services
+            ©
+            {' '}
+            {new Date().getFullYear()}
+            {' '}
+            Dione Protocol LLC
           </p>
         </Link>
-        <Link href="/contact" className={styles.footer_link} needArrow={false}>
-          <p className="sm:text-base text-xs">
-            • Contact
-          </p>
-        </Link>
+        <div className="flex sm:flex-row flex-col justify-start">
+          <p className="sm:text-base text-xs">• All Rights Reserved</p>
+          <Link href="/services" className={styles.footer_link} needArrow={false}>
+            <p className="sm:text-base text-xs">
+              • Term of Services
+            </p>
+          </Link>
+          <Link href="/contact" className={styles.footer_link} needArrow={false}>
+            <p className="sm:text-base text-xs">
+              • Contact
+            </p>
+          </Link>
+        </div>
       </div>
     </div>
     {/* <Typography 
