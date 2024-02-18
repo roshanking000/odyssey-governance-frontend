025defca0f4443f0d8816805c01520b5ef39e1af
CryptoRoshan <118432414+tinyphoniex@users.noreply.github.com>
2024-02-19 04:29:10 +0900

    change footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,7 +8,39 @@ import styles from './styles.module.scss';
 
 const Footer = () => (
   <div className={styles.footer_container}>
-    <div className="flex sm:flex-row gap-8 justify-between items-center flex-col sm:w-full mx-auto">
+    <div className="max-w-full gap-5 lg:gap-0 py-2 md:py-0 px-4 px-0 w-full">
+      <div className="flex justify-between gap-6 lg:gap-12 xl:gap-20 flex-col lg:flex-row items-center w-full">
+        <div className="flex items-center text-sm gap-4 min-w-[135px] lg:h-9 flex-col lg:flex-row pb-6 lg:pb-0 font-medium md:font-normal border-b border-[#ffffff1a] lg:border-b-0 w-full">
+          <Image src={DioneProtocolIcon} alt="logo" width={48} height={48} />
+          <p>Powered by Dione Protocol</p>
+        </div>
+        <div className="flex sm:flex-row items-center flex-col text-nowrap">
+          <Link href="/" className={styles.footer_link} needArrow={false}>
+            <p className="text-xs">
+              ©
+              {' '}
+              {new Date().getFullYear()}
+              {' '}
+              Dione Protocol LLC
+            </p>
+          </Link>
+          <div className="flex sm:flex-row flex-col justify-start">
+            <p className="text-xs">• All Rights Reserved</p>
+            <Link href="/services" className={styles.footer_link} needArrow={false}>
+              <p className="text-xs">
+                • Term of Services
+              </p>
+            </Link>
+            <Link href="/contact" className={styles.footer_link} needArrow={false}>
+              <p className="text-xs">
+                • Contact
+              </p>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+    {/* <div className="flex sm:flex-row gap-8 justify-between items-center flex-col sm:w-full mx-auto">
       <div className="flex sm:flex-row flex-col gap-2 items-center">
         <Image src={DioneProtocolIcon} alt="logo" width={36} height={36} />
         <p>Powered by Dione Protocol</p>
@@ -37,7 +69,7 @@ const Footer = () => (
           </Link>
         </div>
       </div>
-    </div>
+    </div> */}
     {/* <Typography 
       className={ 
         cx(
